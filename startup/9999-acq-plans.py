cb9e79e9a19fcefb73935020d1a27590a68d1d78
Maksim Rakitin <mrakitin@bnl.gov>
2018-09-28 15:04:48 -0400

    There are now 2 plans using the "gas" device
    1) gas + rga masses
    2) gas + rga masses + pe1c detector
    
    To be integrated with xpdAcq

--- startup/9999-acq-plans.py
+++ startup/9999-acq-plans.py
@@ -5,66 +5,93 @@ from xpdacq.beamtime import _configure_area_det
 import os
 import numpy as np
 import itertools
+import bluesky.plans as bp
+import bluesky.plan_stubs as bps
+import bluesky.preprocessors as bpp
+
 from bluesky.plans import (scan, count, list_scan, adaptive_scan)
 from bluesky.preprocessors import subs_wrapper, reset_positions_wrapper
-from bluesky.plan_stubs import abs_set
 from bluesky.callbacks import LiveTable, LivePlot
 from bluesky.plan_tools import print_summary
 
-####  Plan to run Gas/RGA2/pe1c over xpdacq protocols of samples ########
+####  Plan to run Gas/RGA2 over xpdacq protocols of samples ########
 
 gas.gas_list = ['He', 'N2', 'CO2', 'Air']
 
-def Gas_Plan(gas_in = 'He', liveplot_key=None, totExpTime = 5, num_exp = 1, delay = 1):
 
+def gas_plan(gas_in, rga_masses=['mass1', 'mass2', 'mass3', 'mass4', 'mass5', 'mass6']):
     """
-    Execute it
-    ----------
-    >> %run -i /home/xf28id2/Documents/Sanjit/Scripts/GasXrun_Plan.py
-    >> change all the parameters inside Gas_Plan as required
-    >>> gas_plan = Gas_Plan(gas_in = 'He', liveplot_key= 'rga_mass1', totExpTime = 5, num_exp = 3, delay = 1)
-    >> to run the xrun, save metadata & save_tiff run the following
-    >>> run_and_save(sample_num = 0)
+    Example:
 
-    Example
-    -------
-    Set the gasses. They can be in any other, nothing to do with
-    the order they are used in the plan. But, should match the connections on switch.
-    >>> gas.gas_list = ['He', 'N2', 'CO2']
-    >>> RGA mass is set to different value, base shows 1^-13 with He 5 cc flow shows max 2^-8
-    >>> RGA mass setup in mID mode: 4,18,28,31,44,79,94,32,81
+    >>> RE(gas_plan(gas_in='He', masses_to_plot=['mass4', 'mass6']))
+    ----------
+
     Parameters
     ----------
     gas_in : string
         e.g., 'He', default is 'He'
         These gas must be in `gas.gas_list` but they may be in any order.
-    liveplot_key : str, optional
-        e. g., liveplot_key = rga_mass1
-        data key for LivePlot. default is None, which means no LivePlot
-    totExpTime : float
-        total exposure time per frame in seconds. Dafault value is 5 sec
-    num_exp : int
-        number of images/exposure, default is 1
-    delay: float
-        delay time between exposures in sec
-
+    rga_masses: list, optional
+        a list of rga masses appearing in a live table
     """
+    print('Warning: check the gas list!')
+
+    for i in range(1, 9+1):
+        getattr(rga, f'mass{i}').kind = 'normal'
+
+    for m in rga_masses:
+        getattr(rga, m).kind = 'hinted'
 
     ## switch gas
-    yield from abs_set(gas, gas_in)
-
-    ## configure the exposure time first
-    _configure_area_det(totExpTime)   # 5 secs exposuretime
+    yield from bps.mv(gas, gas_in)
 
     ## ScanPlan you need
-    plan = bp.count([pe1c, gas.current_gas, rga], num=num_exp, delay= delay)
+    yield from bp.count([gas.current_gas, rga])
 
-    #plan = bpp.subs_wrapper(plan, LiveTable([xpd_configuration['area_det'], rga]))   # give you LiveTable
-    plan = bpp.subs_wrapper(plan, LiveTable([xpd_configuration['area_det'], gas.current_gas, rga]))
-    if liveplot_key and isinstance(liveplot_key, str):
-        plan =  bpp.subs_wrapper(plan, LivePlot(liveplot_key))
 
-    yield from plan
+def gas_plan_with_detector(gas_in, rga_masses=['mass1', 'mass2', 'mass3', 'mass4', 'mass5', 'mass6'], det=pe1c, exp_time=5, num_exp=1, delay=1):
+    """
+    Example:
+
+    >>> RE(gas_plan(gas_in='He', masses_to_plot=['mass4', 'mass6']))
+    ----------
+
+    Parameters
+    ----------
+    gas_in : string
+        e.g., 'He', default is 'He'
+        These gas must be in `gas.gas_list` but they may be in any order.
+    rga_masses: list, optional
+        a list of rga masses appearing in a live table
+    det : ophyd obj, optional
+        detector to use
+    exp_time : float, optional
+        exposure time in seconds
+    num_exp : integer, optional
+        number of exposures
+    delay : float, optional
+        delay between exposures in seconds
+    """
+    print('Warning: check the gas list!')
+
+    for i in range(1, 9+1):
+        getattr(rga, f'mass{i}').kind = 'normal'
+
+    for m in rga_masses:
+        getattr(rga, m).kind = 'hinted'
+
+    det.stats1.kind = 'hinted'
+    det.stats1.total.kind = 'hinted'
+
+    ## switch gas
+    yield from bps.mv(gas, gas_in)
+
+    # configure the exposure time first
+    _configure_area_det(exp_time)   # secs of exposure time
+
+    ## ScanPlan you need
+    yield from bp.count([gas.current_gas, rga, det], num=num_exp, delay=delay)
+
 
 def run_and_save(sample_num = 0):
     data_dir = "/direct/XF28ID2/pe2_data/xpdUser/tiff_base/"
