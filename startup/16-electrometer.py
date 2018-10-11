6277955a2a0a6648975efeb89388beac3080fde9
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-10-11 12:07:48 -0400

    ENH: use QuadEMV33 from nslsii.ad33 (https://github.com/NSLS-II/nslsii/pull/42)

--- startup/16-electrometer.py
+++ startup/16-electrometer.py
@@ -1,27 +1,7 @@
-from ophyd import Signal, EpicsSignalWithRBV, Component as Cpt
-from ophyd.areadetector import ADBase, ADComponent as ADCpt
-from ophyd import QuadEM
-from nslsii.ad33 import StatsPluginV33
+from nslsii.ad33 import QuadEMV33
 
 
-class QuadEMPort(ADBase):
-    port_name = Cpt(Signal, value="")
-
-    def __init__(self, port_name, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.port_name.put(port_name)
-
-
-class ESMQuadEM(QuadEM):
-    conf = Cpt(QuadEMPort, port_name="EM180")
-    em_range = Cpt(EpicsSignalWithRBV, "Range", string=True)
-
-    current1 = ADCpt(StatsPluginV33, 'Current1:')
-    current2 = ADCpt(StatsPluginV33, 'Current2:')
-    current3 = ADCpt(StatsPluginV33, 'Current3:')
-    current4 = ADCpt(StatsPluginV33, 'Current4:')
-    sum_all = ADCpt(StatsPluginV33, 'SumAll:')
-
+class XPDQuadEM(QuadEMV33):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stage_sigs.update([(self.acquire_mode, "Single")])  # single mode
@@ -46,7 +26,6 @@ class ESMQuadEM(QuadEM):
             cur.mean_value = Kind.hinted
 
 
-qem1 = ESMQuadEM("XF:28IDC-BI{IM:02}EM180:", name="qem1")
+qem1 = XPDQuadEM("XF:28IDC-BI{IM:02}EM180:", name="qem1")
 for det in [qem1]:
     det.read_attrs = ['current2', 'current2.mean_value']
-_
