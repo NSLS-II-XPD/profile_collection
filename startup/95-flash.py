74a5b25062b89f64c9ae6c6916ef9bb515995620
Thomas A Caswell <tcaswell@bnl.gov>
2019-03-20 18:57:04 -0400

    WIP: draft of table plan

--- startup/95-flash.py
+++ startup/95-flash.py
@@ -1,6 +1,13 @@
 from ophyd import (Device,
                    Component as Cpt,
-                   EpicsSignal, EpicsSignalRO)
+                   EpicsSignal, EpicsSignalRO, Signal)
+from bluesky.preprocessors import (
+    run_decorator, stage_decorator, finalize_decorator,
+    monitor_during_decorator)
+import bluesky.plan_stubs as bps
+import pandas as pd
+import time
+
 
 class CurrentSetterEpicSignal(EpicsSignal):
     def stop(self):
@@ -98,3 +105,44 @@ class KeithlyMM(Device):
 
 MM = KeithlyMM(name='MM')
 
+
+def flash_step_field(dets, VIT_table, md, *, delay=1):
+    all_dets = dets + [flash_power, MM]
+    req_cols = ['I', 'V', 't']
+    if not all(k in VIT_table for k in req_cols):
+        raise ValueError(f"input table must have {req_cols}")
+
+    VIT_table = pd.DataFrame(VIT_table)
+    # TODO put in default meta-data
+
+    # paranoia to be very sure we turn the PSU off
+    @finalize_decorator(bps.mov(flash_power.enabled, 0))
+    # this arms the detectors
+    @stage_decorator(all_dets)
+    # this sets up the monitors of the multi-meter
+    @monitor_during_decorator([MM.current, MM.voltage])
+    # this opens the run and puts the meta-data in it
+    @run_decorator(md=md)
+    def flash_step_field_inner():
+        # set everything to zero at the top
+        yield from bps.mv(flash_power.current_sp, 0,
+                          flash_power.voltage_sp, 0)
+        # put in "Duty Cycle" mode so current changes immediately
+        yield from bps.mv(flash_power.mode, 'Duty Cycle')
+        # turn it on!
+        yield from bps.mv(flash_power.enabled, 0)
+
+        for _, row in VIT_table.iterrows():
+            tau = row['t']
+            exposure_count = max(1, tau // delay)
+            yield from bps.mv(flash_power.current, row['I'],
+                              flash_power.voltage, row['V'])
+            deadline = time.monotonic() + tau
+            for j in range(exposure_count):
+                # this triggers the cameras, the PSU readings and the mm
+                yield from bps.trigger_and_read(all_dets)
+                # if things get bogged down in data collection, bail early!
+                if time.monotonic() > deadline:
+                    break
+
+    return (yield from flash_step_field_inner())
