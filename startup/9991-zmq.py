33db91e910553268a2d37899fdd41032d43e8bed
XPD_Operator <xf28id2@xf28id2-ws3.nsls2.bnl.local>
2024-05-14 10:29:05 -0400

    Sync local changes

--- startup/9991-zmq.py
+++ startup/9991-zmq.py
@@ -1,3 +1,4 @@
 from bluesky.callbacks.zmq import Publisher
 
 raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')
+raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')
