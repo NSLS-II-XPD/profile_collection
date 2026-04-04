42c96084169df55995da112139030338869f264e
XPD Operator <xf28id2@xf28id2-ws3.nsls2.bnl.local>
2026-04-03 23:36:57 -0400

    change detectos in md to detectors_in_use

--- startup/94-CHL-plans.py
+++ startup/94-CHL-plans.py
@@ -549,6 +549,9 @@ def xray_uvvis_RE(det1: ophyd.Device,
         mixer (list, optional): list of mixers (example: ['30 cm', '60 cm'])pump_list
         note (str, optional): addtional info. Defaults to None.
     """
+    
+    # print(f'\n===== {[det1.name, det2.name, 'xxxxx'] = } =====\n')
+    
     if (pump_list != None and precursor_list != None):
         _md = {"pumps" : [pump.name for pump in pump_list],
                "precursors" : precursor_list,
@@ -559,7 +562,7 @@ def xray_uvvis_RE(det1: ophyd.Device,
                "mixer": mixer,
                "sample_type": sample_type,
                "sample_name": sample_type,
-               "detectors": [det1.name, det2.name], 
+               "detectors_in_use": [det1.name, det2.name], 
                "note" : note if note else "None"}
         _md.update(md or {})
 
