c962cdc5fe592e2f180f73e5a0f73317c11264da
XPD Operator <xf28id2@xf28id2-ws3.nsls2.bnl.local>
2026-05-12 15:43:41 -0400

    Fixes

--- startup/971-zmq.py
+++ startup/971-zmq.py
@@ -1,7 +1,9 @@
 from bluesky.callbacks.zmq import Publisher
 
 if is_re_worker_active():  # running in queueserver
-    raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')  # used by bluesky-queueserver
+    raw_publisher = Publisher(glbl['inbound_proxy_address'], prefix=b'raw')  # used by bluesky-queueserver
+    RE.subscribe(raw_publisher)
 
 else: # running in bsui
-    raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')  # used in bsui
+    raw_publisher = Publisher(glbl['inbound_proxy_address'], prefix=b'raw')  # used in bsui
+    xrun.subscribe(raw_publisher)
