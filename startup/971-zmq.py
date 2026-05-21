256d984468ef4a536dbdc6180ac20b7086fa845b
XPD Operator <xf28id2@xf28id2-ws3.nsls2.bnl.local>
2026-05-21 19:22:12 -0400

    Update zmq subscription for RE and xrun

--- startup/971-zmq.py
+++ startup/971-zmq.py
@@ -1,4 +1,6 @@
 from bluesky.callbacks.zmq import Publisher
 
-raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=xrun, prefix=b'raw')  # used in bsui
-raw_publisher = Publisher(glbl['inbound_proxy_address'], RE=RE, prefix=b'raw')  # used by bluesky-queueserver
+raw_publisher = Publisher(glbl['inbound_proxy_address'], prefix=b'raw')  # used in bsui
+xrun.subscribe(raw_publisher)
+raw_publisher = Publisher(glbl['inbound_proxy_address'], prefix=b'raw')  # used by bluesky-queueserver
+RE.subscribe(raw_publisher)
